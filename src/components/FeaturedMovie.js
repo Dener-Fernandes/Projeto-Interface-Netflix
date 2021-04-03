8b76f832df1a08117074b8f3bcac20770d80582e
Dener Fernandes <dener.fernandes.oliveira@gmail.com>
2021-04-03 10:25:04 -0400

    Projeto Interface da Netflix, desenvolvido com ReactJS.

---
+++ src/components/FeaturedMovie.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./FeaturedMovie.css";
+
+export default ({ item }) => {
+
+    let firstDate = new Date(item.first_air_date);
+    let genres = [];
+    for (let i in item.genres) {
+        genres.push(item.genres[i].name);
+    }
+
+    // let description = item.overview;
+    // if (description.length > 50) {
+    //     description = description.substring(0, 100)+"...";
+    // }
+
+    return (
+        <section className="featured" style={{
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+        }}>
+            <div className="featured--vertical">
+                <div className="featured--horizontal">
+                    <div className="featured--name"><div>{item.original_name}</div></div>
+                    <div className="featured--info">
+                        <div className="featured--points">{item.vote_average} pontos</div>
+                        <div className="featured--year">{firstDate.getFullYear()}</div>
+                        <div className="featured--seasons">{item.nuber_of_seasons} temporada{item.nuber_of_seasons !== 1 ? "s" : ""}</div>
+                    </div>
+                    <div className="featured--description">{item.overview}</div>
+                    <div className="featured--buttons">
+                        <a href={`/watch/${item.id}`} className="featured--watchButton">► Assistir</a>
+                        <a href={`/list/add/${item.id}`} className="featured--myListButton">+ Minha Lista</a>
+                    </div>
+                    <div className="featured--genres"><strong>Gêneros: {genres.join(", ")}</strong></div>
+                </div>
+            </div>
+        </section>
+    );
+}
